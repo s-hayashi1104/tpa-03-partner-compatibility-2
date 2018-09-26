706d6184fb496c49b933051dc4dc77b0dde6189e
s-hayashi1104 <shibahas.has@gmail.com>
2018-09-27 02:22:03 +0900

    ・Implementation of calculation logic ・Implementation of calculation logic test

--- compatibility-calculator.js
+++ compatibility-calculator.js
@@ -1,28 +1,46 @@
 const { candidates } = require('./data/candidates-data.js');
 
-const calculateBestMatch = function(quizSubmissions) {
 
-  // TODO: replace this...
-  return candidates[0];
+const points = (candidatePoints, clientInputValue) => {
+  if (candidatePoints === clientInputValue) {
+    return 2;
+  }
+  else if (candidatePoints + 1 === clientInputValue || candidatePoints - 1 === clientInputValue) {
+    return 1;
+  }
+  return 0;
+};
 
-  // TODO: implement the following algorithm for
-  // calculating the candidate who is the best match for you
+const calculateBestMatch = (quizSubmissions) => {
 
-  // set closest match to 1st candidate
-
-  // for each candidate
-  //  for each question
-  //    if question values are exactly the same
-  //      add 2 points
-  //    if question values are one score away
-  //      add 1 point
-  //  update the closest match
-
-  // for ties, the last person who has the highest score is returned
-
-  // return closest match
+  // 一人目はどれだけ点数が低くてもマッチさせる
+  let match = candidates[0];
+  let score = 0;
+  // 点数計算３重ネストリストを要素の数だけ回す
+  // 人
+  candidates.forEach((candidate) => {
+    let candidateScore = 0;
+    // オブジェクトを内包した配列
+    candidate.forEach((quizData, first) => {
+      // オブジェクト
+      quizData.forEach((quiz, second) => {
+        if (quiz.name.startsWith('question')) {
+          const reqObj = quizSubmissions[first][second];
+          const candidatePoints = parseInt(quiz.value);
+          const clientInputValue = parseInt(reqObj.value);
+          candidateScore += points(candidatePoints, clientInputValue);
+        }
+      });
+    });
+    if (candidateScore >= score) {
+      match = candidate;
+      score = candidateScore;
+    }
+  });
+  return match;
 };
 
 module.exports = {
   calculateBestMatch,
+  points,
 };
--- compatibility-calculator.test.js
+++ compatibility-calculator.test.js
@@ -1,3 +1,34 @@
-const { calculateBestMatch } = require('./compatibility-calculator');
-
+const { calculateBestMatch, points } = require('./compatibility-calculator');
+const { candidates } = require('./data/candidates-data.js');
 // your test code here
+
+function expects(expectation, actual, message = 'テスト') {
+  if (expectation !== actual){
+    throw `${message}:期待値と実際の値が一致しない。expectation: ${expectation}, actual: ${actual}`;
+  }
+  console.log(`${message}:成功した。`);
+}
+
+function expectBestMatch(expectation, actual, message = 'テスト') {
+  const strExpectation = JSON.stringify(expectation);
+  const strActual = JSON.stringify(actual);
+  if (strExpectation !== strActual){
+    throw `${message}:期待値と実際の値が一致しない。expectation: ${strExpectation}, actual: ${strActual}`;
+  }
+  console.log(`${message}:成功した。`);
+}
+
+
+expects(points(1, 1), 2, 'return 2');
+expects(points(2, 1), 1, 'return 1');
+expects(points(1, 2), 1, 'return 1');
+expects(points(4, 0), 0, 'return 0');
+expects(points(1, 3), 0, 'return 0');
+
+
+expectBestMatch(calculateBestMatch(candidates[0])).toEqual(candidates[0]);
+expectBestMatch(calculateBestMatch(candidates[1])).toEqual(candidates[1]);
+expectBestMatch(calculateBestMatch(candidates[2])).toEqual(candidates[2]);
+expectBestMatch(calculateBestMatch(candidates[3])).toEqual(candidates[3]);
+
+console.log('全て成功した。');
